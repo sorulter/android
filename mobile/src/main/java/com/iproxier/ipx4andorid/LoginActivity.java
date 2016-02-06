5936713c68e896be21e799ce5f75c34319a5ac0b
Frank Kung <kfanjian@gmail.com>
2016-02-07 00:53:24 +0800

    Create empty Dashboard Activity.

--- mobile/src/main/java/com/iproxier/ipx4andorid/LoginActivity.java
+++ mobile/src/main/java/com/iproxier/ipx4andorid/LoginActivity.java
@@ -3,6 +3,7 @@ package com.iproxier.ipx4andorid;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -187,6 +188,10 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             showProgress(true);
             mAuthTask = new UserLoginTask(email, password);
             mAuthTask.execute((Void) null);
+            if (mAuthTask.doInBackground()) {
+                Intent intent = new Intent(this, DashboardActivity.class);
+                startActivity(intent);
+            }
         }
     }
 
@@ -324,7 +329,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             }
 
             // TODO: register the new account here.
-            return true;
+            return false;
         }
 
         @Override
