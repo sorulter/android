4495a66618db4f75262e9500533b4f831ae99af6
Frank Kung <kfanjian@gmail.com>
2016-02-07 00:36:33 +0800

    Create init project with Login Activity in Android Studio.

---
+++ mobile/src/androidTest/java/com/iproxier/ipx4andorid/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.iproxier.ipx4andorid;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
