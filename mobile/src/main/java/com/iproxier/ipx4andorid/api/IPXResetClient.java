69a1455a885b56e2a1243780f1a94badc26fcf33
Frank Kung <kfanjian@gmail.com>
2016-02-12 12:00:01 +0800

    Modify IPX reset client to sync.

--- mobile/src/main/java/com/iproxier/ipx4andorid/api/IPXResetClient.java
+++ mobile/src/main/java/com/iproxier/ipx4andorid/api/IPXResetClient.java
@@ -1,20 +1,19 @@
 package com.iproxier.ipx4andorid.api;
 
-import com.loopj.android.http.AsyncHttpClient;
-import com.loopj.android.http.AsyncHttpResponseHandler;
-import com.loopj.android.http.RequestHandle;
+import com.loopj.android.http.JsonHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
+import com.loopj.android.http.SyncHttpClient;
 
 public class IPXResetClient {
     private static final String BASE_URL="http://10.211.55.15/api/v1/";
-    private static AsyncHttpClient client = new AsyncHttpClient();
+    private static SyncHttpClient client = new SyncHttpClient();
 
-    public static RequestHandle get(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
-        return client.get(getAbsoluteUrl(url), params, responseHandler);
+    public static void get(String url, RequestParams params, JsonHttpResponseHandler responseHandler) {
+        client.get(getAbsoluteUrl(url), params, responseHandler);
     }
 
-    public static RequestHandle post(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
-        return client.post(getAbsoluteUrl(url), params, responseHandler);
+    public static void post(String url, RequestParams params, JsonHttpResponseHandler responseHandler) {
+        client.post(getAbsoluteUrl(url), params, responseHandler);
     }
 
     private  static String getAbsoluteUrl(String relativeUrl) {
