c661d4d892a556b73bdde057c571d43b116d3c1a
Frank Kung <kfanjian@gmail.com>
2016-02-19 01:31:24 +0800

    Remove dashboard content ,back home button and floating action button.

--- mobile/src/main/java/com/iproxier/ipx4andorid/DashboardActivity.java
+++ mobile/src/main/java/com/iproxier/ipx4andorid/DashboardActivity.java
@@ -20,15 +20,6 @@ public class DashboardActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         SharedPreferences sharedPref = DashboardActivity.this.getSharedPreferences("IPX_API_TOKEN", Context.MODE_PRIVATE);
     }
